afc59a6433a3c6813f3181cb27a00b85335103fe
Darina1984 <daria.salina@testpro.io>
2023-03-26 08:59:14 -0400

    homework16-1

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -10,7 +10,7 @@ import java.time.Duration;
 
 public class LoginTests extends BaseTest {
     @Test
-    public void successfulLoginTest()  {
+    public void registrationNavigation()  {
 
 //      Added ChromeOptions argument below to fix websocket error
         ChromeOptions options = new ChromeOptions();
@@ -21,81 +21,19 @@ public class LoginTests extends BaseTest {
 
         String url = "https://bbb.testpro.io/";
         driver.get(url);
-        // email
-        WebElement emailInput = driver.findElement(By.xpath("//input[@type='email']"));
-        emailInput.click();
-        emailInput.clear();
-        emailInput.sendKeys("denis.grubenko@gmail.com");
-        // password
-        WebElement passwordInput = driver.findElement(By.cssSelector("[type='password']"));
-        passwordInput.click();
-        passwordInput.clear();
-        passwordInput.sendKeys("te$t$tudent");
-        // login button
-        WebElement submitLoginButton = driver.findElement(By.cssSelector("button[type='submit']"));
-        submitLoginButton.click();
+        // registration button
+        WebElement registrationButt = driver.findElement(By.cssSelector("[id='hel']---"));
+        registrationButt.click();
 
-        WebElement avatar = driver.findElement(By.cssSelector("a .avatar"));
-        Assert.assertTrue(avatar.isDisplayed());
+        // text register new account
+        WebElement textNewAccount = driver.findElement(By.cssSelector("[style='padding: 32px 0; text-align: center;']"));
+        Assert.assertTrue(textNewAccount.isDisplayed());
 
         driver.quit();
     }
 
-    @Test
-    public void wrongPasswordLoginTest() {
 
-//      Added ChromeOptions argument below to fix websocket error
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--remote-allow-origins=*");
-
-        WebDriver driver = new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-
-        String url = "https://bbb.testpro.io/";
-        driver.get(url);
-        // email
-        WebElement emailInput = driver.findElement(By.xpath("//input[@type='email']"));
-        emailInput.click();
-        emailInput.clear();
-        emailInput.sendKeys("demo@class.com");
-        // password
-        WebElement passwordInput = driver.findElement(By.cssSelector("[type='password']"));
-        passwordInput.click();
-        passwordInput.clear();
-        passwordInput.sendKeys("te$t$tuden");
-        // login button
-        WebElement submitLoginButton = driver.findElement(By.cssSelector("button[type='submit']"));
-        submitLoginButton.click();
-        Assert.assertTrue(submitLoginButton.isDisplayed());
-        driver.quit();
     }
 
-    @Test
-    public void emptyPasswordLoginTest(){
 
-//      Added ChromeOptions argument below to fix websocket error
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--remote-allow-origins=*");
 
-        WebDriver driver = new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-
-        String url = "https://bbb.testpro.io/";
-        driver.get(url);
-        // email
-        WebElement emailInput = driver.findElement(By.xpath("//input[@type='email']"));
-        emailInput.click();
-        emailInput.clear();
-        emailInput.sendKeys("demo@class.com");
-        // password
-        WebElement passwordInput = driver.findElement(By.cssSelector("[type='password']"));
-        passwordInput.click();
-        passwordInput.clear();
-        passwordInput.sendKeys("");
-        // login button
-        WebElement submitLoginButton = driver.findElement(By.cssSelector("button[type='submit']"));
-        submitLoginButton.click();
-        Assert.assertTrue(submitLoginButton.isDisplayed());
-        driver.quit();
-    }
-}
