c000d05dc346770080396037998d9776cb9175b1
Darina1984 <daria.salina@testpro.io>
2023-03-29 19:14:07 -0400

    class new-5

--- src/test/java/Homework18.java
+++ src/test/java/Homework18.java
@@ -7,9 +7,25 @@ import org.testng.annotations.Test;
 
 public class Homework18 extends BaseTest{
     @Test
-    public void playSong() {
+    public void playSong() throws InterruptedException {
+        //login
        login("dsalina1984@gmail.com","Qazxsw123@Qazxsw123@");
 
+        // click all songs button
+        WebElement allSongsButton = driver.findElement(By.cssSelector("[href='#!/songs']"));
+        Thread.sleep(1000);
+        allSongsButton.click();
+
+        // play a song
+
+        WebElement clickable = driver.findElement(By.xpath("//td[contains(text(),'Ketsa - That_s a Beat')]"));
+        new Actions(driver)
+                .doubleClick(clickable)
+                .perform();
+
+        //assert equalizer appeared the song is plaing
+        WebElement equalizerButton = driver.findElement(By.cssSelector("[alt='Sound bars']"));
+        Assert.assertTrue(equalizerButton.isDisplayed());
 
     }
 
