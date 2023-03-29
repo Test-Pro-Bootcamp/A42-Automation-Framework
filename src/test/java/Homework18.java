ffb15366c8b1c23251d191bc4d7f55cac123e2f5
Darina1984 <daria.salina@testpro.io>
2023-03-29 15:37:18 -0400

    class new-4

--- src/test/java/Homework18.java
+++ src/test/java/Homework18.java
@@ -1,3 +1,18 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class Homework18 extends BaseTest{
+    @Test
+    public void playSong() {
+       login("dsalina1984@gmail.com","Qazxsw123@Qazxsw123@");
+
+
+    }
 
 }
+
+
