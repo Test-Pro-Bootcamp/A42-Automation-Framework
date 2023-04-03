53a74a4633646d44b9e9313c782004c4b5b2822c
LidiiaSinikova <lidiia.sinikova@testpro.io>
2023-04-03 08:02:34 -0400

    changes-2

--- src/test/java/Homework20.java
+++ src/test/java/Homework20.java
@@ -1,15 +1,11 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import java.time.Duration;
+
 
 public class Homework20 extends BaseTest{
     @Test
