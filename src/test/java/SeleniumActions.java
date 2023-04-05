a0b80f500faa9e8fb9c87c3fb11c5e4cb3c4fde7
Darina1984 <daria.salina@testpro.io>
2023-04-05 19:29:23 -0400

    create and delete playlist-homework20

---
+++ src/test/java/SeleniumActions.java
@@ -0,0 +1,29 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.annotations.Test;
+
+public class SeleniumActions extends BaseTest {
+    @Test
+    public void playListSong (){
+        login("dsalina1984@gmail.com", "Qazxsw123@Qazxsw123@");
+        chooseAllSongsList();
+        contextClickFirstSong();
+        choosePlay();
+    }
+
+    // helper methods
+    public void chooseAllSongsList (){
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("li a.songs" ))).click();
+    }
+    public void contextClickFirstSong(){
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".all-songs tr.song-item:nth-child(1)")));
+        WebElement firstSong = driver.findElement(By.cssSelector(".all-songs tr.song-item:nth-child(1)"));
+        actions.contextClick(firstSong).perform();
+    }
+    public void choosePlay(){
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".playback"))).click();
+    }
+}
+
+
