dd18f592228f7a673baf12de67c7c75f5b46c16d
Darina1984 <daria.salina@testpro.io>
2023-04-06 08:19:39 -0400

    selenium playlistSong

--- src/test/java/SeleniumActions.java
+++ src/test/java/SeleniumActions.java
@@ -1,29 +1,38 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class SeleniumActions extends BaseTest {
     @Test
-    public void playListSong (){
+    public void playListSong() {
         login("dsalina1984@gmail.com", "Qazxsw123@Qazxsw123@");
         chooseAllSongsList();
         contextClickFirstSong();
         choosePlay();
+        Assert.assertTrue(isSongPlaying());
+        
+    }
+    // helper methods
+    public void chooseAllSongsList() {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("li a.songs"))).click();
     }
 
-    // helper methods
-    public void chooseAllSongsList (){
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("li a.songs" ))).click();
-    }
-    public void contextClickFirstSong(){
+    public void contextClickFirstSong() {
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".all-songs tr.song-item:nth-child(1)")));
         WebElement firstSong = driver.findElement(By.cssSelector(".all-songs tr.song-item:nth-child(1)"));
         actions.contextClick(firstSong).perform();
     }
-    public void choosePlay(){
+
+    public void choosePlay() {
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".playback"))).click();
     }
+    private boolean isSongPlaying() {
+        WebElement soundBarVisualizer = driver.findElement(By.cssSelector("[data-testid = 'sound-bar-play']"));
+        return soundBarVisualizer.isDisplayed();
+    }
+   
 }
 
 
