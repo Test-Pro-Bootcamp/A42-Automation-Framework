1da646fccc4f51b8029741a2612e272e39569fa7
victorcolodzei <128403606+victorcolodzei@users.noreply.github.com>
2023-04-13 17:20:53 +0300

    Homework24

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,13 +1,13 @@
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
 import pages.BasePage;
 import pages.LoginPage;
 import pages.PlaylistPage;
 import pages.SongPage;
+
+import java.net.MalformedURLException;
 
 public class BaseTest {
     public BasePage basePage = new BasePage();
@@ -16,13 +16,13 @@ public class BaseTest {
     public SongPage songPage;
     String url = "https://bbb.testpro.io/";
 
-    @BeforeSuite
-    static void setupClass() {
-        WebDriverManager.chromedriver().setup();
-    }
+//    @BeforeSuite
+//    static void setupClass() {
+//        WebDriverManager.chromedriver().setup();
+//    }
 
     @BeforeMethod
-    void setUpBrowser() {
+    void setUpBrowser() throws MalformedURLException {
         basePage.initBrowser(url);
         loginPage = new LoginPage(basePage.getDriver());
         playlistPage = new PlaylistPage(basePage.getDriver());
--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -1,15 +1,22 @@
 package pages;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.safari.SafariDriver;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.net.MalformedURLException;
+import java.net.URI;
 import java.time.Duration;
 
 
@@ -33,13 +40,44 @@ public class BasePage {
         return driver;
     }
 
-    public void initBrowser(String url) {
+    public void initBrowser(String url) throws MalformedURLException {
         //Added ChromeOptions argument below to fix websocket error
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--disable-notifications", "--remote-allow-origins=*", "--incognito", "--start-maximized");
-        driver = new ChromeDriver(options);
+        driver = pickBrowser(System.getProperty("browser"));
         driver.manage().window().maximize();
         driver.get(url);
+    }
+
+    public WebDriver pickBrowser(String browser) throws MalformedURLException {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        String gridURL = "http://192.168.1.241:4444";
+        switch (browser) {
+            case "firefox" -> {
+                WebDriverManager.firefoxdriver().setup();
+                return driver = new FirefoxDriver();
+            }
+            case "safari" -> {
+                WebDriverManager.safaridriver().setup();
+                return driver = new SafariDriver();
+            }
+            case "grid-firefox" -> {
+                capabilities.setCapability("browserName", "firefox");
+                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(), capabilities);
+            }
+            case "grid-safari" -> {
+                capabilities.setCapability("browserName", "safari");
+                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(), capabilities);
+            }
+            case "grid-chrome" -> {
+                capabilities.setCapability("browserName", "chrome");
+                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(), capabilities);
+            }
+            default -> {
+                WebDriverManager.chromedriver().setup();
+                ChromeOptions options = new ChromeOptions();
+                options.addArguments("--disable-notifications", "--remote-allow-origins=*", "--incognito", "--start-maximized");
+                return driver = new ChromeDriver(options);
+            }
+        }
     }
 
     public void closeBrowser() {
