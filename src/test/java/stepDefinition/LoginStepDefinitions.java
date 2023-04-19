4430bc7be246e1db84bc9989c0e9c8317189f188
LidiiaSinikova <lidiia.sinikova@testpro.io>
2023-04-19 10:41:45 -0400

    HW-26-changes

--- src/test/java/stepDefinition/LoginStepDefinitions.java
+++ src/test/java/stepDefinition/LoginStepDefinitions.java
@@ -7,23 +7,27 @@ import io.cucumber.java.en.When;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
 import java.time.Duration;
 
-public class LoginStepDefinitions {
-    WebDriver driver;
+public class LoginStepDefinitions{
     WebDriverWait wait;
+    WebDriver driver;
     @Given("I open browser")
     public void openBrowser(){
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-notifications","--remote-allow-origins=*", "--incognito","--start-maximized");
+        WebDriver driver = new ChromeDriver(options);
         wait = new WebDriverWait(driver, Duration.ofSeconds(5));
     }
 @And("I open Login Page")
     public void openLoginPage(){
-        driver.get("https://bbb.testpro.io");
+       driver.get("https://bbb.testpro.io");
+
 }
 @When("I enter email{string}")
     public void enterEmail(String email){
@@ -35,12 +39,12 @@ public class LoginStepDefinitions {
 }
 @And("I submit")
     public void submit(){
-        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("'[type='submit']"))).click();
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("button[type='submit']"))).click();
 
 }
 @Then("I am logged in")
   public void userIsLoggedIn(){
-     Assert.assertTrue(wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("img.avatar"))).isDisplayed());
+     Assert.assertTrue(wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("a .avatar"))).isDisplayed());
 }
 
 }
