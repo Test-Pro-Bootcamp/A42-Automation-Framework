6e61ad1356723c3957d7260a1b9c490df8c0bc4c
LidiiaSinikova <lidiia.sinikova@testpro.io>
2023-04-18 22:10:57 -0400

    HW26

--- src/test/java/stepDefinition/LoginStepDefinitions.java
+++ src/test/java/stepDefinition/LoginStepDefinitions.java
@@ -1,17 +1,46 @@
 package stepDefinition;
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 
 import java.time.Duration;
 
 public class LoginStepDefinitions {
-    Webdriver driver;
+    WebDriver driver;
     WebDriverWait wait;
     @Given("I open browser")
     public void openBrowser(){
         driver = new ChromeDriver();
         wait = new WebDriverWait(driver, Duration.ofSeconds(5));
     }
+@And("I open Login Page")
+    public void openLoginPage(){
+        driver.get("https://bbb.testpro.io");
+}
+@When("I enter email{string}")
+    public void enterEmail(String email){
+       wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("[type='email']"))).sendKeys("lidiia@northins.com");
+}
+@And("I enter password{string}")
+    public void enterPassword(String password){
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("[type='password']"))).sendKeys("Lidiia1807@");
+}
+@And("I submit")
+    public void submit(){
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("'[type='submit']"))).click();
+
+}
+@Then("I am logged in")
+  public void userIsLoggedIn(){
+     Assert.assertTrue(wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("img.avatar"))).isDisplayed());
+}
+
 }
--- src/test/resources/Features/Login/Login.feature
+++ src/test/resources/Features/Login/Login.feature
@@ -3,7 +3,7 @@ Feature: Login feature
   Scenario: Login Success
     Given I open browser
     And I open Login Page
-    When I enter email"demo@class.com"
-    And I enter password "te$t$tudent"
+    When I enter email"lidiia@northins.com"
+    And I enter password"Lidiia1807@"
     And I submit
     Then I am logged in
